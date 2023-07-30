5c21382af1eede9d5e4e0ba2902cae1e3c2bc866
Анна Жжонова <annzhzho@gmail.com>
2023-07-30 18:15:49 +0400

    add styles

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 
 const sectionTest = document.querySelector('.test');
 const selectList = document.querySelector('.select__list');
-async function getPlanet () {
+const inputNumber = document.querySelector('.input__number');
+
+async function getPlanets () {
     try {
-        const response = await fetch('https://swapi.dev/api/planets')
-        const data = await response.json()
-        const planets = data.results
-        console.log(planets)
+        const response = await fetch('https://swapi.dev/api/planets');
+        const data = await response.json();
+        const planets = await data.results;
+        const planetName = await planets[inputNumber.value - 1].name;
+        selectedResult.textContent = planetName;
     }
     catch (error){
         console.error(error)
@@ -15,28 +18,49 @@ async function getPlanet () {
 
 async function getPeople (){
    try{
-    const response = await fetch('https://swapi.dev/api/people')
-    const data = await response.json()
-    const characters = data.results
-    console.log(characters)
+    const response = await fetch('https://swapi.dev/api/people');
+    const data = await response.json();
+    const characters = await data.results;
+    const charactersName = await characters[inputNumber.value - 1].name;
+    selectedResult.textContent = charactersName;
     }
     catch (error){
         console.error(error);
     }
-    
 }
 
 async function getFilms (){
     try{
         const response = await fetch('https://swapi.dev/api/films')
-        const data = await response.json()
-        const films = data.results
-        console.log(films)
+        const data = await response.json();
+        const films = await data.results;
+        const filmsName =  await films[inputNumber.value - 1].title;
+        selectedResult.textContent = filmsName;
     }
     catch(error){
         console.error(error)
     }
 }
-getPlanet();
-getPeople();
-getFilms();
+
+
+const optionPlanets = document.getElementById('planets');
+const optionCharacters = document.getElementById('characters');
+const optionFilms = document.getElementById('films');
+const selectedResult = document.querySelector('.result');
+
+function showSelectedName () {
+    if(optionPlanets.selected){
+        getPlanets()
+    }
+     if (optionCharacters.selected) {
+        getPeople ()
+    }
+    if (optionFilms.selected){
+        getFilms ()
+    }
+}
+
+document.querySelector('.btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    showSelectedName();
+});
